5cba5cf5bf2b9e61b7c955645bc77df19ff5981d
ozan <123476414+ozan2604@users.noreply.github.com>
2025-03-21 01:05:41 +0300

    Created appuser, approle entities and implemented jwt token architecture

--- YourBlogAPI/Infrastructure/Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ YourBlogAPI/Infrastructure/Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73248f0ae2ad866406bf2dbf3e4ba5709c483fd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52fea0896e165177268c1e92d978e5bd1e2097f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
